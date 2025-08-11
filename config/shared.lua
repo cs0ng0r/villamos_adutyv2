0ec319c4d2168ad49713d4a2cf1fc661184ea67a
cs0ng0r <62070315+cs0ng0r@users.noreply.github.com>
2025-08-11 17:38:09 +0200

    Update shared.lua

--- config/shared.lua
+++ config/shared.lua
@@ -13,38 +13,6 @@ Config.Icons = {
     "marvel"
 }
 
-local clothes = {
-    male = {
-        ['helmet_1'] = 91,
-        ['helmet_2'] = 0,
-        ['tshirt_1'] = 15,
-        ['tshirt_2'] = 0,
-        ['torso_1'] = 178,
-        ['torso_2'] = 0,
-        ['decals_1'] = 0,
-        ['decals_2'] = 0,
-        ['arms'] = 1,
-        ['pants_1'] = 77,
-        ['pants_2'] = 0,
-        ['shoes_1'] = 55,
-        ['shoes_2'] = 0,
-    },
-    female = {
-        ['helmet_1'] = 114,
-        ['helmet_2'] = 24,
-        ['tshirt_1'] = 14,
-        ['tshirt_2'] = 0,
-        ['torso_1'] = 180,
-        ['torso_2'] = 5,
-        ['decals_1'] = 0,
-        ['decals_2'] = 0,
-        ['arms'] = 14,
-        ['pants_1'] = 79,
-        ['pants_2'] = 5,
-        ['shoes_1'] = 58,
-        ['shoes_2'] = 5,
-    }
-}
 
 Config.Perms = {
     "god",
@@ -53,9 +21,9 @@ Config.Perms = {
 }
 
 Config.Admins = { --a pedet vagy a logót vagy a ruhát ha nem szeretnéd használni állítsd falsera
-    ["god"] = { tag = "[GOD]", logo = "marvel", ped = false, cloth = clothes, color = { r = 162, g = 0, b = 0 } },
-    ["admin"] = { tag = "[ADMIN]", logo = "marvel", ped = false, cloth = clothes, color = { r = 162, g = 0, b = 0 } },
-    ["mod"] = { tag = "[MOD]", logo = "marvel", ped = `s_m_m_chemsec_01`, cloth = false, color = { r = 162, g = 0, b = 0 } },
+    ["god"] = { tag = "[GOD]", logo = "marvel", color = { r = 162, g = 0, b = 0 } },
+    ["admin"] = { tag = "[ADMIN]", logo = "marvel", color = { r = 162, g = 0, b = 0 } },
+    ["mod"] = { tag = "[MOD]", logo = "marvel", color = { r = 162, g = 0, b = 0 } },
 }
 
 Config.Notify = function(msg)
@@ -73,3 +41,4 @@ Config.Notify = function(msg)
         })
     end
 end
+
